440cefc847cd189426e1cdece15d091c2ea3bbe9
harunamarun <harunamarun@gmail.com>
2020-02-10 21:59:39 +0900

    added GET /api/redumes/:id

--- server/models/resumes/get.js
+++ server/models/resumes/get.js
@@ -1,17 +1,12 @@
-// module.exports = (knex, Todo) => {
-//   return params => {
-//     const id = params.id;
-//     const title = params.title;
-//     const filter = {};
-//     if (id !== undefined) filter.id = id;
-//     if (title !== undefined) filter.title = title;
-
-//     return knex("todos")
-//       .where(filter)
-//       .select()
-//       .then(todos => {
-//         if (todos.length) return new Todo(todos.pop());
-//         throw new Error(`Error finding todo ${id}`);
-//       });
-//   };
-// };
+module.exports = (knex, Resume) => {
+  return params => {
+    const id = params.id;
+    return knex("resumes")
+      .where({ id })
+      .select()
+      .then(resumes => {
+        if (resumes.length) return new Resume(resumes.pop());
+        throw new Error(`Error finding todo ${id}`);
+      });
+  };
+};
