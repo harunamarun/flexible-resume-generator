4983545c5d18bba378019fabbb53f8c60d65b531
harunamarun <harunamarun@gmail.com>
2020-02-12 14:44:57 +0900

    fixed layout

--- src/components/ResumeList.jsx
+++ src/components/ResumeList.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function ResumeList(props) {
   return (
-    <div>
+    <div id="resume-list">
       {props.resumes.map(resume => {
         return (
           <li
